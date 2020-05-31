461f074082b888e9e054f5e9ef18d90bb2e77566
Nicolae Orlov <Nicu104@gmail.com>
2020-05-31 18:07:03 -0500

    other changes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
 import json
+from sqlalchemy import func
+import random
 
 
 app = Flask(__name__)
@@ -85,12 +87,33 @@ def getUsers():
     return jsonify(all_users)
 
 
+@app.route('/api/username')
+def getUserByName():
+    name = request.args.get('name')
+    if not name or name == "":
+        all_users = [x.as_dict() for x in Users.query.order_by(Users.created_at).all()]
+    else:
+        all_users = [x.as_dict() for x in Users.query.filter(func.lower(Users.name) == func.lower(name)).order_by(Users.created_at)]
+    return jsonify(all_users)
+
+
+@app.route('/api/userid')
+def getUserById():
+    id = request.args.get('id')
+    if not id or id == "":
+        return jsonify({"error": "There is an error"})
+    else:
+        user = Users.query.get_or_404(id).as_dict()
+        # user = Users.query.filter_by(id=id).as_dict()
+    return jsonify(user)
+
+
 @app.route('/user', methods=["POST", "GET"])
 def users():
     if request.method == "POST":
-        name = request.form["name"]
-        location = request.form["location"]
-        salary = request.form["salary"]
+        name = request.form["Name"]
+        location = request.form["Location"]
+        salary = request.form["Salary"]
         new_user = Users(name=name, location=location, salary=salary)
         try:
             db.session.add(new_user)
@@ -102,9 +125,43 @@ def users():
         return render_template("newUser.html")
 
 
+@app.route('/api/user', methods=["POST"])
+def api_users():
+    data = request.json
+    name = data["Name"]
+    location = data["Location"]
+    salary = data["Salary"]
+    new_user = Users(name=name, location=location, salary=salary)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"status":"success", "statusCode":200})
+    except:
+        return jsonify({"status":"error", "statusCode":400})
+
+
+@app.route('/api/department')
+def api_department():
+    data = [
+        {"Id":1, "Name":"QTST", "Revenue":1500},
+        {"Id":2, "Name":"Devs", "Revenue":1000},
+        {"Id":3, "Name":"OPS", "Revenue":2000},
+        {"Id":4, "Name":"CIA", "Revenue":5000},
+        {"Id":5, "Name":"FBI", "Revenue":6000}
+    ]
+    return jsonify(data)
+
+
+@app.route('/api/numberofdays')
+def numberofdays():
+    numdays = random.randint(1,5000)
+    return jsonify(numdays)
+
+
+
 # to create database open python from curent directory 
 # from app import db
 # db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
